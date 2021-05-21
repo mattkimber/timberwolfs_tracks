61bb05ca3097e34001c802aff6308b818a192c73
Matt Kimber <github@mattkimber.org.uk>
2021-05-21 13:41:06 +0100

    alignments wip (to reset)

---
+++ nml.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Assemble the output using Roadie
+../roadie/roadie.exe set.json
+
+# NML
+../nml/nmlc.exe -c timberwolfs_tracks.nml
+
+echo "Building TAR"
+mkdir -p timberwolfs_tracks
+mv *.grf timberwolfs_tracks
+cp grf_readme/* timberwolfs_tracks
+tar -c timberwolfs_tracks > timberwolfs_tracks.tar
